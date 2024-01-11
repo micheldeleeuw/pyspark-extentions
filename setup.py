13b8eb082e31b7ba5fd97001daf2a699a84d19b2
michelwehkamp <mdleeuw@wehkamp.nl>
2024-01-11 16:29:20 +0100

    automatic commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+import os
+
+from setuptools import setup, find_packages
+from process import about
+from dunamai import Version
+
+version = Version.from_git()
+
+setup(
+    name=about.__title__,
+
+    # specify version as environment variable, so we can
+    # use it for preview builds, otherwise fall back to default
+    version=os.getenv('VERSION', f'{version.base}+{version.distance}.{version.commit}'),
+
+    packages=find_packages(exclude=['tests', 'tests.*', 'images']),
+    include_package_data=True,
+
+    install_requires=[
+        'wheel>=0.42.0',
+        # 'databricks-cli==0.18.0',
+        'pyspark==3.4.1',
+    ],
+    author=about.__author__,
+    author_email=about.__mail__
+)
