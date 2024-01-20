24f3e7ffb9bd6cb3ba659d1908a49646576964e1
michelwehkamp <mdleeuw@wehkamp.nl>
2024-01-20 17:53:57 +0100

    automatic commit Sat Jan 20 17:53:57 CET 2024

--- pyspark_extensions/config/__init__.py
+++ pyspark_extensions/config/__init__.py
@@ -4,7 +4,7 @@ import os
 import pathlib
 
 config_path = pathlib.Path(__file__).parent.absolute().parent.absolute().parent.absolute() / "config.ini"
-logging.debug(f'config.ini path: {config_path}')
+print(f'config.ini path: {config_path}')
 config = ConfigParser()
 config.read(config_path)
 
