8275939e91c757c2c946fc586b5cc553c9b739f9
michelwehkamp <mdleeuw@wehkamp.nl>
2024-01-20 17:51:37 +0100

    automatic commit Sat Jan 20 17:51:37 CET 2024

--- pyspark_extensions/config/__init__.py
+++ pyspark_extensions/config/__init__.py
@@ -1,8 +1,10 @@
+import logging
 from configparser import ConfigParser
 import os
 import pathlib
 
 config_path = pathlib.Path(__file__).parent.absolute().parent.absolute().parent.absolute() / "config.ini"
+logging.debug(f'config.ini path: {config_path}')
 config = ConfigParser()
 config.read(config_path)
 
--- tests/methods/test_update_table.py
+++ tests/methods/test_update_table.py
@@ -5,4 +5,4 @@ import pyspark.sql.functions as F
 def test_update_table(spark, test_set_1):
     test_set_1.eUpdateTable('test_update', 'line_id')
 
-    add proper test, check location
+    # add proper test, check location
